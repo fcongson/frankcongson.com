b538c53768ed48a82b2e820689a63aaf1430c04d
Frank Congson <fcongson@gmail.com>
2020-03-01 21:20:39 +1300

    Add queries to pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,42 @@
+import { graphql } from 'gatsby'
+import { RichText } from 'prismic-reactjs'
 import React from 'react'
 import Layout from '../components/layouts'
 
-export default () => {
+export const query = graphql`
+  {
+    prismic {
+      allAbouts(uid: "about") {
+        edges {
+          node {
+            _meta {
+              id
+              type
+            }
+            page_header
+          }
+        }
+      }
+    }
+  }
+`
+
+const About = ({ about }) => (
+  <div className='section'>
+    <div className='container'>
+      <h1 className='section-header'>{RichText.asText(about.page_header)}</h1>
+    </div>
+  </div>
+)
+
+export default ({ data }) => {
+  const doc = data.prismic.allAbouts.edges.slice(0, 1).pop()
+
+  if (!doc) return null
+
   return (
     <Layout>
-      <div className='section'>
-        <div className='container'>
-          <h1 className='section-header'>About.</h1>
-        </div>
-      </div>
+      <About about={doc.node} />
     </Layout>
   )
 }
--- src/pages/photography.js
+++ src/pages/photography.js
@@ -1,14 +1,42 @@
+import { graphql } from 'gatsby'
+import { RichText } from 'prismic-reactjs'
 import React from 'react'
 import Layout from '../components/layouts'
 
-export default () => {
+export const query = graphql`
+  {
+    prismic {
+      allPhotographys(uid: "photography") {
+        edges {
+          node {
+            _meta {
+              id
+              type
+            }
+            page_header
+          }
+        }
+      }
+    }
+  }
+`
+
+const Photography = ({ photography }) => (
+  <div className='section'>
+    <div className='container'>
+      <h1 className='section-header'>{RichText.asText(photography.page_header)}</h1>
+    </div>
+  </div>
+)
+
+export default ({ data }) => {
+  const doc = data.prismic.allPhotographys.edges.slice(0, 1).pop()
+
+  if (!doc) return null
+
   return (
     <Layout>
-      <div className='section'>
-        <div className='container'>
-          <h1 className='section-header'>Photography.</h1>
-        </div>
-      </div>
+      <Photography photography={doc.node} />
     </Layout>
   )
 }
