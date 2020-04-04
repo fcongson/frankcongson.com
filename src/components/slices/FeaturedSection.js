b98b181648c18dfa3c2202d8f81fc7efd70bbbe4
Frank Congson <fcongson@gmail.com>
2020-04-04 22:41:50 +1300

    Update home page sections

--- src/components/slices/FeaturedSection.js
+++ src/components/slices/FeaturedSection.js
@@ -1,56 +1,134 @@
-import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 import { RichText } from 'prismic-reactjs'
 import React from 'react'
 import styled from 'styled-components'
 import htmlSerializer from '../../utils/htmlSerializer'
 import { linkResolver } from '../../utils/linkResolver'
-import { Container, Section } from '../styles'
+import { Container, LinkButton, Section } from '../styles'
 
-const FeaturedSectionContent = styled.div`
-  .featured-image {
-    width: 100%;
+const FeaturedContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+
+  ${props =>
+    props.imageBackground &&
+    `
+      height: 100vh;
+
+      div.image {
+        grid-area: 1 / 1;
+        height: 100vh;
+        opacity: 0.5;
+      }
+
+      ${Section} {
+        z-index: 1;
+      }
+    `}
+
+  ${props =>
+    props.colorBackground &&
+    `
+      background-color: ${props.theme.colors.greyLight40};
+
+      ${Container} {
+        padding-top: 8rem;
+        padding-bottom: 8rem;
+      }
+    `}
+
+  ${Section} {
+    grid-area: 1 / 1;
+    align-self: center;
   }
 
-  .featured-container {
+  ${Container} {
+    margin-bottom: 0;
+  }
+
+  div.image {
+    width: 100%;
+  }
+  
+  div.text {
     display: flex;
     flex-direction: column;
     justify-content: center;
+    
+    p {
+      text-align: center;
+    }
   }
 
-  .call-to-action {
+  ${LinkButton} {
     margin: 0 auto;
   }
 `
 
-export default ({ slice }) => {
+// Image Background
+const ImageBackground = ({ slice }) => {
   return (
-    <FeaturedSectionContent>
+    <FeaturedContainer imageBackground>
       {!!slice.primary.section_imageSharp && (
-        <Section>
-          <Container>
-            <div className='featured-image'>
+        <div className='image'>
+          <Img
+            fluid={slice.primary.section_imageSharp.childImageSharp.fluid}
+            alt={slice.primary.section_image.alt}
+            style={{ height: '100%' }}
+            imgStyle={{ objectPosition: 'center center' }}
+          />
+        </div>
+      )}
+      <Section>
+        <Container>
+          <div className='text'>
+            <h2>{RichText.asText(slice.primary.section_header, linkResolver, htmlSerializer)}</h2>
+            <p>{RichText.asText(slice.primary.section_text, linkResolver, htmlSerializer)}</p>
+            <LinkButton to={`/${slice.primary.call_to_action._meta.uid}`}>
+              {RichText.asText(slice.primary.call_to_action_text, linkResolver, htmlSerializer)}
+            </LinkButton>
+          </div>
+        </Container>
+      </Section>
+    </FeaturedContainer>
+  )
+}
+
+// Color Background
+const ColorBackground = ({ slice }) => {
+  return (
+    <FeaturedContainer colorBackground>
+      <Section>
+        <Container>
+          {!!slice.primary.section_imageSharp && (
+            <div className='image'>
               <Img
                 fluid={slice.primary.section_imageSharp.childImageSharp.fluid}
                 alt={slice.primary.section_image.alt}
               />
             </div>
-          </Container>
-        </Section>
-      )}
-      <Section>
-        <Container>
-          <div className='featured-container'>
-            <h2 className='section-header'>
-              {RichText.asText(slice.primary.section_header, linkResolver, htmlSerializer)}
-            </h2>
-            <p className='section-text'>{RichText.asText(slice.primary.section_text, linkResolver, htmlSerializer)}</p>
-            <Link className='call-to-action button' to={`/${slice.primary.call_to_action._meta.uid}`}>
+          )}
+          <div className='text'>
+            <h2>{RichText.asText(slice.primary.section_header, linkResolver, htmlSerializer)}</h2>
+            <p>{RichText.asText(slice.primary.section_text, linkResolver, htmlSerializer)}</p>
+            <LinkButton to={`/${slice.primary.call_to_action._meta.uid}`}>
               {RichText.asText(slice.primary.call_to_action_text, linkResolver, htmlSerializer)}
-            </Link>
+            </LinkButton>
           </div>
         </Container>
       </Section>
-    </FeaturedSectionContent>
+    </FeaturedContainer>
   )
 }
+
+const renderSwitch = slice => {
+  switch (slice.label) {
+    case 'image_background':
+      return <ImageBackground slice={slice} />
+    case 'color_background':
+    default:
+      return <ColorBackground slice={slice} />
+  }
+}
+
+export default ({ slice }) => renderSwitch(slice)
--- src/components/styles/index.js
+++ src/components/styles/index.js
@@ -2,4 +2,5 @@ import theme from './theme'
 export default theme
 
 export * from './Container'
+export * from './LinkButton'
 export * from './Section'
