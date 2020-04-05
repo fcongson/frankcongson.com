fd1b33ec769ea3a9489173f4ba76d3fd5ca67986
Frank Congson <fcongson@gmail.com>
2020-04-05 21:41:38 +1200

    Update layout components to use styled components

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,12 +1,64 @@
 import { faGithub, faInstagram, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import styled from 'styled-components'
 import logo from '../../images/logo-light.svg'
+import { Container, Section } from '../styles'
+
+const Footer = styled.footer`
+  background-color: ${props => props.theme.colors.black};
+  color: ${props => props.theme.colors.greyLight40};
+  text-align: center;
+  flex-shrink: 0;
+
+  ${Container} {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+    min-height: 128px;
+    margin: 0 auto;
+  }
+
+  .footer-logo {
+    height: 64px;
+    width: 64px;
+  }
+
+  .footer-content {
+    display: flex;
+    flex-direction: column;
+    text-align: right;
+
+    .social-links {
+      font-size: 1.5rem;
+      margin-right: -1rem;
+      padding: 1rem 0 1rem 0;
+
+      & a {
+        padding: 1rem;
+      }
+
+      & svg {
+        width: 1.5rem;
+        height: 1.5rem;
+        color: ${props => props.theme.colors.greyLight40};
+      }
+    }
+
+    .copyright {
+      font-style: normal;
+      font-weight: normal;
+      font-size: 12px;
+      margin: 0;
+    }
+  }
+`
 
 export default ({ social }) => (
-  <footer>
-    <div className='section'>
-      <div className='container footer-container'>
+  <Footer>
+    <Section>
+      <Container>
         <img className='footer-logo' src={logo} alt='Frank Congson logo' />
         <div className='footer-content'>
           <div className='social-links'>
@@ -25,7 +77,7 @@ export default ({ social }) => (
           </div>
           <p className='copyright'>Copyright © {new Date().getFullYear()} Frank Congson</p>
         </div>
-      </div>
-    </div>
-  </footer>
+      </Container>
+    </Section>
+  </Footer>
 )
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,19 +1,155 @@
 import { Link } from 'gatsby'
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import logo from '../../images/logo.svg'
+import { Container, Section } from '../styles'
 
-export default ({ page }) => {
+const Header = styled.header`
+  display: block;
+  z-index: 1000;
+
+  ${Container} {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 0 auto 4rem auto;
+
+    @media (max-width: ${props => props.theme.breakpoints.maxWidthTabletLandscape}) {
+      margin: 0 auto;
+      flex-wrap: wrap;
+    }
+  }
+
+  .logo-container {
+    height: 64px;
+  }
+`
+
+const NavigationMenu = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: stretch;
+
+  @media (max-width: ${props => props.theme.breakpoints.maxWidthTabletLandscape}) {
+    flex-direction: column;
+    margin-top: 2rem;
+    order: 2;
+    width: 100%;
+    height: 0;
+    overflow: hidden;
+    transition: height 200ms ease-in-out, padding-bottom 200ms ease-in-out;
+
+    ${props =>
+      props.open &&
+      `
+      height: 312px;
+      padding-bottom: 4rem;
+      `}
+
+    ${props =>
+      props.overlay &&
+      `
+        position: absolute;
+        top: 80px;
+      `}
+  }
+
+  .navigation-link {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+    width: fit-content;
+    margin: 0 0 0 32px;
+    padding: 30px 0 0 0;
+    border-bottom: 2px solid transparent;
+    transition: border-bottom 200ms ease-in-out;
+    color: ${props => props.theme.colors.greyDark40};
+
+    &:hover {
+      border-bottom: 2px solid ${props => props.theme.colors.greyDark40};
+    }
+
+    &[aria-current='page'] {
+      border-bottom: 2px solid ${props => props.theme.colors.greyDark40};
+    }
+
+    @media (max-width: ${props => props.theme.breakpoints.maxWidthTabletLandscape}) {
+      font-size: 36px;
+      line-height: 44px;
+      margin: 0 0 8px 0;
+      padding: 0 0 8px 0;
+
+      &:hover {
+        border-bottom: 2px solid ${props => props.theme.colors.greyDark40};
+      }
+
+      &[aria-current='page'] {
+        border-bottom: 2px solid ${props => props.theme.colors.greyDark40};
+      }
+    }
+  }
+`
+
+const NavigationToggle = styled.button`
+  display: none;
+  height: 64px;
+
+  .toggle-line {
+    position: relative;
+    width: 32px;
+    height: 2px;
+    background-color: ${props => props.theme.colors.greyDark40};
+    transition: transform 100ms ease-in-out 0ms, top 100ms ease-in-out 100ms;
+
+    &.top {
+      top: 1px;
+    }
+
+    &.bottom {
+      top: 13px;
+    }
+
+    ${props =>
+      props.open &&
+      `
+        transition: top 100ms ease-in-out 0ms, transform 100ms ease-in-out 100ms;
+        
+        &.top {
+          top: 8px;
+          transform: rotate(45deg);
+        }
+
+        &.bottom {
+          top: 6px;
+          transform: rotate(-45deg);
+        }
+      `}
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  @media (max-width: ${props => props.theme.breakpoints.maxWidthTabletLandscape}) {
+    display: block;
+    order: 1;
+  }
+`
+
+export default ({ navigationOverlay }) => {
   const [open, setOpen] = useState(false)
   return (
-    <header>
-      <div className='section'>
-        <div className='container header-container'>
+    <Header>
+      <Section>
+        <Container>
           <div className='logo-container'>
             <Link to='/'>
-              <img className='header-logo' src={logo} alt='Frank Congson logo' />
+              <img src={logo} alt='Frank Congson logo' />
             </Link>
           </div>
-          <div className={`navigation-container${open ? ' open' : ''}${page === 'home' ? ' overlay' : ''}`}>
+          <NavigationMenu open={open} overlay={navigationOverlay}>
             <Link className='navigation-link' to='/'>
               Home
             </Link>
@@ -26,16 +162,16 @@ export default ({ page }) => {
             <Link className='navigation-link' to='/blog'>
               Blog
             </Link>
-          </div>
-          <button
+          </NavigationMenu>
+          <NavigationToggle
             aria-label={`${open ? 'Close' : 'Open'} navigation menu`}
-            className='navigation-menu-toggle'
-            onClick={() => setOpen(!open)}>
-            <div className={`toggle-line top${open ? ' open' : ''}`} />
-            <div className={`toggle-line bottom${open ? ' open' : ''}`} />
-          </button>
-        </div>
-      </div>
-    </header>
+            onClick={() => setOpen(!open)}
+            open={open}>
+            <div className='toggle-line top' />
+            <div className='toggle-line bottom' />
+          </NavigationToggle>
+        </Container>
+      </Section>
+    </Header>
   )
 }
