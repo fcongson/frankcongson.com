b721a8e2b9e7e148821f26bde53c1c0a25e46013
Frank Congson <fcongson@gmail.com>
2020-04-11 21:26:51 +1200

    Update photography page layout

--- src/pages/photography.js
+++ src/pages/photography.js
@@ -50,28 +50,66 @@ export const query = graphql`
   }
 `
 
-const PhotographyContainer = styled.div`
-  .photography-image {
+const PhotographyImage = styled.div`
+  height: 100vh;
+  display: grid;
+  align-items: center;
+  margin: 0 auto 4rem auto;
+
+  @media (max-width: ${(props) => props.theme.breakpoints.maxWidthTabletPortrait}) {
+    margin: 0 auto 2rem auto;
+  }
+
+  ${Container} {
+    display: flex;
+    flex-direction: row;
+    margin: 0 auto;
+
+    @media (max-width: ${(props) => props.theme.breakpoints.maxWidthTabletPortrait}) {
+      flex-direction: column-reverse;
+    }
+  }
+
+  div.description {
+    width: 160px;
+    background-color: ${(props) => props.theme.colors.greyDark40};
+    color: ${(props) => props.theme.colors.greyLight40};
+    font-size: 36px;
+    line-height: 44px;
+    padding-top: 2rem;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+
+    @media (max-width: ${(props) => props.theme.breakpoints.maxWidthTabletPortrait}) {
+      width: 100%;
+      height: 64px;
+      font-size: 24px;
+      line-height: 29px;
+      padding: 1rem;
+    }
+  }
+
+  div.image {
     width: 100%;
-    padding: 0 4rem;
+    max-height: calc(100vh - 4rem); /* 100vh - margins */
+    margin: 0 0 0 2rem;
 
-    &:not(:last-of-type) {
-      padding-bottom: 2rem;
+    picture {
+      object-position: left center;
     }
 
-    @media (max-width: ${props => props.theme.breakpoints.maxWidthTabletLandscape}) {
-      padding: 0 2rem;
-
-      &:not(:last-of-type) {
-        padding-bottom: 1rem;
-      }
+    @media (max-width: ${(props) => props.theme.breakpoints.maxWidthTabletLandscape}) {
+      max-height: calc(100vh - 2rem); /* 100vh - margins */
+      margin: 0 0 0 1rem;
     }
 
-    @media (max-width: ${props => props.theme.breakpoints.maxWidthMobileLandscape}) {
-      padding: 0;
+    @media (max-width: ${(props) => props.theme.breakpoints.maxWidthTabletPortrait}) {
+      max-height: calc(100vh - 3rem - 64px); /* 100vh - margins - description height */
+      margin: 0 0 1rem 0;
 
-      &:not(:last-of-type) {
-        padding-bottom: 0.5rem;
+      picture {
+        object-position: center center;
       }
     }
   }
@@ -105,22 +143,30 @@ const Photography = ({ photography }) => {
         }
         content={<PageHeader>{RichText.asText(photography.page_header)}</PageHeader>}
       />
-      <PhotographyContainer>
-        <Section>
-          <Container>
-            <InfiniteScroll
-              dataLength={images.length}
-              next={() => dispatch({ type: 'next' })}
-              hasMore={images.length < photography.images.length}>
-              {images.map(({ image, imageSharp }) => (
-                <div className='photography-image' key={image.url}>
-                  <Img fluid={imageSharp.childImageSharp.fluid} alt={image.alt} />
+      <InfiniteScroll
+        dataLength={images.length}
+        next={() => dispatch({ type: 'next' })}
+        hasMore={images.length < photography.images.length}>
+        {images.map(({ image, imageSharp }, index) => (
+          <PhotographyImage key={image.url}>
+            <Section>
+              <Container>
+                <div className='description'>
+                  {index + 1} / {photography.images.length}
                 </div>
-              ))}
-            </InfiniteScroll>
-          </Container>
-        </Section>
-      </PhotographyContainer>
+                <div className='image'>
+                  <Img
+                    fluid={imageSharp.childImageSharp.fluid}
+                    alt={image.alt}
+                    style={{ maxHeight: 'inherit' }}
+                    imgStyle={{ objectFit: 'contain', objectPosition: 'inherit' }}
+                  />
+                </div>
+              </Container>
+            </Section>
+          </PhotographyImage>
+        ))}
+      </InfiniteScroll>
     </>
   )
 }
