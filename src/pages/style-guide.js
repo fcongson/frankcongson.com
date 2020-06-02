c72771ab830c20836778702b15ff0a65e9a24f13
Frank Congson <fcongson@gmail.com>
2020-06-02 21:40:55 +1200

    Update quote blocks

--- src/pages/style-guide.js
+++ src/pages/style-guide.js
@@ -200,7 +200,25 @@ export default () => (
     <Section>
       <Container id='quote'>
         <Quote>Short quote.</Quote>
+        <Text>
+          <p>
+            Paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum.
+          </p>
+        </Text>
         <Quote>Medium quote. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Quote>
+        <Text>
+          <p>
+            Paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum.
+          </p>
+        </Text>
         <Quote>
           Long quote. Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore
           et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
