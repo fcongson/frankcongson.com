ceb60db5510f17c994c04cfc926e41293177e415
Frank Congson <fcongson@gmail.com>
2020-11-07 22:44:47 +1300

    Update styled headers

--- src/components/styles/PageHeader.js
+++ src/components/styles/PageHeader.js
@@ -1,22 +1,17 @@
 import styled from 'styled-components'
+import { space, typography } from 'styled-system'
 
 export const PageHeader = styled.h1`
-  font-family: ${(props) => props.theme.fonts.sansSerif};
-  font-style: normal;
-  font-weight: bold;
-  font-size: 121.5px;
-  line-height: 148px;
-  text-align: center;
-  margin: 0 auto 4rem auto;
-
-  @media (max-width: ${(props) => props.theme.breakpoints[3]}) {
-    font-size: 81px;
-    line-height: 99px;
-    margin: 0 auto 2rem auto;
-  }
-
-  @media (max-width: ${(props) => props.theme.breakpoints[1]}) {
-    font-size: 54px;
-    line-height: 66px;
-  }
+  ${typography}
+  ${space}
 `
+
+PageHeader.defaultProps = {
+  fontFamily: 'sansSerif',
+  fontWeight: 'bold',
+  fontSize: [6, null, 7, null, 8],
+  lineHeight: [3, null, 4, null, 5],
+  textAlign: 'center',
+  margin: '0 auto',
+  marginBottom: [4, null, null, null, 5],
+}
--- src/components/styles/SectionHeader.js
+++ src/components/styles/SectionHeader.js
@@ -1,20 +1,17 @@
 import styled from 'styled-components'
+import { space, typography } from 'styled-system'
 
 export const SectionHeader = styled.h2`
-  font-weight: bold;
-  font-size: 81px;
-  line-height: 99px;
-  text-align: center;
-  margin: 0 auto 4rem auto;
-
-  @media (max-width: ${(props) => props.theme.breakpoints[3]}) {
-    font-size: 54px;
-    line-height: 66px;
-    margin: 0 auto 2rem auto;
-  }
-
-  @media (max-width: ${(props) => props.theme.breakpoints[1]}) {
-    font-size: 36px;
-    line-height: 44px;
-  }
+  ${typography}
+  ${space}
 `
+
+SectionHeader.defaultProps = {
+  fontFamily: 'sansSerif',
+  fontWeight: 'bold',
+  fontSize: [5, null, 6, null, 7],
+  lineHeight: [2, null, 3, null, 4],
+  textAlign: 'center',
+  margin: '0 auto',
+  marginBottom: [4, null, null, null, 5],
+}
