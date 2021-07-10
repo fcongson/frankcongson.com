68c1c20773c4afd87cb28bed25a6b287db498cbf
Frank Congson <fcongson@gmail.com>
2021-07-10 19:58:34 +1200

    Add motion to photography

--- src/pages/photography.tsx
+++ src/pages/photography.tsx
@@ -3,12 +3,14 @@ import { Hero } from 'components/Hero'
 import { Layout } from 'components/layouts'
 import { SEO as Seo } from 'components/SEO'
 import photography from 'content/data/photography.json'
+import { motion } from 'framer-motion'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import React from 'react'
+import { useInView } from 'react-intersection-observer'
 import styled from 'styled-components'
 import { useImage } from 'utils/useImage'
 
-const PhotographyImage = styled.div<{ label: string }>`
+const PhotographyImage = styled(motion.div)<{ label: string }>`
   display: grid;
   align-items: center;
 
@@ -50,9 +52,19 @@ const Photography: React.FunctionComponent = () => {
         <PageHeader>{photography.page_header}</PageHeader>
       </Hero>
       {images.map(({ image, alt_text, caption }, index) => {
+        const { ref: imageRef, inView: imageInView } = useInView({ threshold: 0.2, triggerOnce: true })
         const imageSharp = getImage(image)
         return (
-          <PhotographyImage key={image} label={`${index + 1} / ${totalImages}`}>
+          <PhotographyImage
+            key={image}
+            label={`${index + 1} / ${totalImages}`}
+            ref={imageRef}
+            initial='hidden'
+            animate={imageInView ? 'visible' : 'hidden'}
+            variants={{
+              visible: { translateY: 0, opacity: 1, transition: { ease: 'linear', duration: 0.5 } },
+              hidden: { translateY: 100, opacity: 0 },
+            }}>
             <Container display='flex' flexDirection='row'>
               <div className='image'>
                 <GatsbyImage image={imageSharp?.childImageSharp?.gatsbyImageData} alt={alt_text} />
