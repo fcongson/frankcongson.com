8e1390ad8e14e7d9c29c56c64efb8182fffa9694
Frank Congson <fcongson@gmail.com>
2021-11-15 09:31:34 +1300

    Remove netlify plugin gatsby cache

--- graphql-types.ts
+++ graphql-types.ts
@@ -236,6 +236,8 @@ export type DirectoryCtimeArgs = {
 export type Site = Node & {
   buildTime?: Maybe<Scalars['Date']>
   siteMetadata?: Maybe<SiteSiteMetadata>
+  port?: Maybe<Scalars['Int']>
+  host?: Maybe<Scalars['String']>
   polyfill?: Maybe<Scalars['Boolean']>
   pathPrefix?: Maybe<Scalars['String']>
   jsxRuntime?: Maybe<Scalars['String']>
@@ -858,6 +860,8 @@ export type QueryAllDirectoryArgs = {
 export type QuerySiteArgs = {
   buildTime?: Maybe<DateQueryOperatorInput>
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>
+  port?: Maybe<IntQueryOperatorInput>
+  host?: Maybe<StringQueryOperatorInput>
   polyfill?: Maybe<BooleanQueryOperatorInput>
   pathPrefix?: Maybe<StringQueryOperatorInput>
   jsxRuntime?: Maybe<StringQueryOperatorInput>
@@ -2368,6 +2372,8 @@ export type SiteFieldsEnum =
   | 'siteMetadata___youtubeUrl'
   | 'siteMetadata___linkedinUrl'
   | 'siteMetadata___githubUrl'
+  | 'port'
+  | 'host'
   | 'polyfill'
   | 'pathPrefix'
   | 'jsxRuntime'
@@ -2497,6 +2503,8 @@ export type SiteGroupConnectionGroupArgs = {
 export type SiteFilterInput = {
   buildTime?: Maybe<DateQueryOperatorInput>
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>
+  port?: Maybe<IntQueryOperatorInput>
+  host?: Maybe<StringQueryOperatorInput>
   polyfill?: Maybe<BooleanQueryOperatorInput>
   pathPrefix?: Maybe<StringQueryOperatorInput>
   jsxRuntime?: Maybe<StringQueryOperatorInput>
