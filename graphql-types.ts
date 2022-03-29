e45f3ff3d40f93a8f59a27762cc9e0cbd70d8d97
Frank Congson <fcongson@gmail.com>
2022-03-29 22:09:52 +1300

    Add blog post - design systems thinking

--- graphql-types.ts
+++ graphql-types.ts
@@ -245,6 +245,8 @@ export type DirectoryCtimeArgs = {
 export type Site = Node & {
   buildTime?: Maybe<Scalars['Date']>
   siteMetadata?: Maybe<SiteSiteMetadata>
+  port?: Maybe<Scalars['Int']>
+  host?: Maybe<Scalars['String']>
   polyfill?: Maybe<Scalars['Boolean']>
   pathPrefix?: Maybe<Scalars['String']>
   jsxRuntime?: Maybe<Scalars['String']>
@@ -962,6 +964,8 @@ export type QueryAllDirectoryArgs = {
 export type QuerySiteArgs = {
   buildTime?: InputMaybe<DateQueryOperatorInput>
   siteMetadata?: InputMaybe<SiteSiteMetadataFilterInput>
+  port?: InputMaybe<IntQueryOperatorInput>
+  host?: InputMaybe<StringQueryOperatorInput>
   polyfill?: InputMaybe<BooleanQueryOperatorInput>
   pathPrefix?: InputMaybe<StringQueryOperatorInput>
   jsxRuntime?: InputMaybe<StringQueryOperatorInput>
@@ -2886,6 +2890,8 @@ export type SiteFieldsEnum =
   | 'siteMetadata___youtubeUrl'
   | 'siteMetadata___linkedinUrl'
   | 'siteMetadata___githubUrl'
+  | 'port'
+  | 'host'
   | 'polyfill'
   | 'pathPrefix'
   | 'jsxRuntime'
@@ -3016,6 +3022,8 @@ export type SiteGroupConnectionGroupArgs = {
 export type SiteFilterInput = {
   buildTime?: InputMaybe<DateQueryOperatorInput>
   siteMetadata?: InputMaybe<SiteSiteMetadataFilterInput>
+  port?: InputMaybe<IntQueryOperatorInput>
+  host?: InputMaybe<StringQueryOperatorInput>
   polyfill?: InputMaybe<BooleanQueryOperatorInput>
   pathPrefix?: InputMaybe<StringQueryOperatorInput>
   jsxRuntime?: InputMaybe<StringQueryOperatorInput>
