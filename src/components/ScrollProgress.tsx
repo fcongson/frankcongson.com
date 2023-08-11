b6642c2eb66d52ae20da5a25326bf20c7b94d9b0
Frank Congson <fcongson@gmail.com>
2023-08-11 16:01:54 +1200

    Abstract reading progress

---
+++ src/components/ScrollProgress.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import { useScrollPosition } from 'utils/useScrollPosition'
+
+const StyledScrollProgress = styled.div<{ progress?: number }>`
+  position: fixed;
+  top: 0;
+  height: var(--lagom-core-spacing-md);
+  width: ${(props) => `${props.progress ?? 0}%`};
+  background-color: var(--lagom-semantic-color-accent-bg);
+  z-index: 2000;
+  transition: width 100ms ease;
+`
+
+export const ScrollProgress = () => {
+  const { handleScroll, scrollProgress } = useScrollPosition()
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  return <StyledScrollProgress progress={scrollProgress} />
+}
