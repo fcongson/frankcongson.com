f6815ad766bb2a394928dbf4f418d67643ff43b7
Frank Congson <fcongson@gmail.com>
2023-09-12 14:16:45 +1200

    Fix font flicker

--- src/components/styles/GlobalStyle.ts
+++ src/components/styles/GlobalStyle.ts
@@ -5,11 +5,6 @@ import '@fcongson/lagom-tokens/css/theme/_light.css'
 import '@fcongson/lagom-tokens/css/variables/_core.css'
 
 export const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'silkamono';
-    src: url('/fonts/silkamono-regular-webfont.woff2') format('woff2');
-  }
-
   html {
     background-color: var(--lagom-semantic-color-bg-default);
   }
